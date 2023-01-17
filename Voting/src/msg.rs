5fa0c6b063133514c0d28c17593757d77ddf24e6
WildHashGhost-Cica <wildhashghost@gmail.com>
2023-01-17 11:00:53 +0000

    msg

--- Voting/src/msg.rs
+++ Voting/src/msg.rs
@@ -1,30 +1,58 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
+use crate::state::{Poll, Votes};
+
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub struct InstantiateMsg {
-    pub val: String,
+    pub admin: Option<String>,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
-    CustomMsg { val: String },
+    CreatePoll {
+        poll_id: String,
+        topic: String,
+        options: Vec<String>
+    },
+    Vote {
+        poll_id:String,
+        vote: String,
+    }
+
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
-    CustomMsg { val: String },
+    AllPolls {},
+    Poll{ poll_id:String },
+    Vote { poll_id: String, address:String },
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
-pub struct CustomResponse {
-    val: String,
+pub struct AllPollResponse {
+    
+    pub polls: Vec<Poll>,
 }
 
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub struct PollResponse {
+    
+    pub poll: Option<Poll>,
+}
+
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub struct VoteResponse {
+    
+    pub vote: Vec<Votes>,
+}
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum MigrateMsg {}
